2ff9044ea7a79b212b899b3a4881f0f681d3d0db
John Gavin <john.b.gavin@gmail.com>
2024-05-23 20:58:50 +0100

    added usethis::use_import_from cmds

--- albert.rapp.de/R/albert.rapp.de-package.R
+++ albert.rapp.de/R/albert.rapp.de-package.R
@@ -2,5 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom ggiraph girafe
+#' @importFrom giscoR gisco_get_nuts
+#' @importFrom rix rix
+#' @importFrom sf st_within
 ## usethis namespace: end
 NULL
